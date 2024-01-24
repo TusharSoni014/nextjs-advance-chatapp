8438c66500149daacbe22592c53681585cf80e49
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-24 21:22:15 +0530

    added db connection with upstash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-"use client"
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { db } from "@/lib/db";
 
-export default function Home() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const loadingHandler = () => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  };
+export default async function Home() {
+  await db.set("hello", "hello world");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button loading={loading} onClick={loadingHandler}>
-        awdff
-      </Button>
+      <Button>test button</Button>
     </main>
   );
 }
