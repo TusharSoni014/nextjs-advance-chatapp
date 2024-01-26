8c331d251d6a9318347f33f9a5fd7113159b8103
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-26 16:22:55 +0530

    fixed google oauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth/next";
 
-export default NextAuth(authOptions);
+const NextAuthHandler = NextAuth(authOptions);
+export { NextAuthHandler as GET, NextAuthHandler as POST };
