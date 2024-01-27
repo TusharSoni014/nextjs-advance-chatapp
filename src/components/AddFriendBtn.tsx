644f56873a13e9afffbc184d89d3837b494be98a
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-27 18:55:08 +0530

    created add friend form with shadcn form

--- src/components/AddFriendBtn.tsx
+++ src/components/AddFriendBtn.tsx
@@ -3,21 +3,58 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  email: z.string().email(),
+});
 
 const AddFriendBtn = () => {
-  const addFriend = async () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+  
+  const addFriend = async ({ email }: z.infer<typeof formSchema>) => {
     try {
-      
-    } catch (error) {
-      
-    }
+      await axios.post("/api/friends/add", {
+        email: email,
+      });
+    } catch (error) {}
   };
 
   return (
-    <form onSubmit={addFriend} className="flex gap-2">
-      <Input placeholder="email/username of your friend" />
-      <Button type="submit">Send Request</Button>
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(addFriend)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Email of your friend" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Send Request</Button>
+      </form>
+    </Form>
   );
 };
 
