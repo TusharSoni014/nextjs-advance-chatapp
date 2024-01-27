e12bb300bbf9ba60b2f83bdbf2a06eeb5dea3326
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-27 18:14:07 +0530

    added add friend btn

--- src/components/AddFriendBtn.tsx
+++ src/components/AddFriendBtn.tsx
@@ -5,10 +5,16 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 const AddFriendBtn = () => {
-  const addFriend = async (email: string) => {};
+  const addFriend = async () => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  };
 
   return (
-    <form className="flex gap-2">
+    <form onSubmit={addFriend} className="flex gap-2">
       <Input placeholder="email/username of your friend" />
       <Button type="submit">Send Request</Button>
     </form>
