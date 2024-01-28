28524824d03fdef2535c7b928203ef0eb3360eb7
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-01-28 18:14:20 +0530

    improved error handling in addfriend btn

--- src/components/AddFriendBtn.tsx
+++ src/components/AddFriendBtn.tsx
@@ -1,45 +1,65 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { toast } from "./ui/use-toast";
 
 const formSchema = z.object({
   email: z.string().email(),
 });
 
 const AddFriendBtn = () => {
+  const [showSuccessState, setShowSuccessState] = useState<boolean>(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
     },
   });
-  
+
   const addFriend = async ({ email }: z.infer<typeof formSchema>) => {
     try {
       await axios.post("/api/friends/add", {
         email: email,
       });
-    } catch (error) {}
+      setShowSuccessState(true);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        form.setError("email", { message: error.message });
+        toast({
+          variant: "destructive",
+          description: error.message,
+        });
+      }
+      if (error instanceof AxiosError) {
+        form.setError("email", { message: error.response?.data });
+        toast({
+          variant: "destructive",
+          description: error.response?.data,
+        });
+      }
+      toast({
+        variant: "destructive",
+        description: "Something went wrong! Please try again",
+      });
+    }
   };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(addFriend)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(addFriend)} className="flex gap-2">
         <FormField
           control={form.control}
           name="email"
