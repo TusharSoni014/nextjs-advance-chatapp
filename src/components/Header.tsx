975087352986a618bfcb6c6edb059df17b3c49c1
Tushar Soni <71227235+TusharSoni014@users.noreply.github.com>
2024-02-07 19:56:31 +0530

    minor header change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,12 @@ export default function Header() {
   return (
     <nav className="__nav_bar h-[60px] flex justify-between px-5 items-center bg-slate-900">
       <h2 className=" font-bold text-xl">NextChat</h2>
-      <div className="__nav_links">
+      <div className="__nav_links flex gap-1">
         <Link href="/login">
           <Button>Login</Button>
+        </Link>
+        <Link href="/dashboard">
+          <Button>Dashboard</Button>
         </Link>
       </div>
     </nav>
